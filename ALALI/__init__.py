e3fa061290470e17db1d7fb097136a505eab591b
Jack Goon <jackbgoon@Jacks-MacBook-Pro-2.local>
2020-07-13 17:54:20 -0700

    initial commit

---
+++ ALALI/__init__.py
@@ -0,0 +1,4 @@
+from ALALI.Chemistry import *
+from ALALI.IBMLayouts import *
+from ALALI.Constraint import *
+__version__ = "0.0.1"
---
+++ README.md
@@ -0,0 +1,2 @@
+# ALALI
+Automated Loading assuming Atomic Level Interactions: A python tool to map atoms to qubits
